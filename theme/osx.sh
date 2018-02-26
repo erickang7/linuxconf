076052a23063867021e03f92caee6988e0bcd636
erickang7 <erickang7@live.com>
2018-02-25 18:03:30 -0800

    .

--- theme/osx.sh
+++
@@ -1,8 +0,0 @@
-mkdie -p ~/.themes
-cd ~/.themes
-
-wget https://github.com/B00merang-Project/macOS-Sierra/archive/master.zip
-unip master.zip
-
-gsettings set org.gnome.desktop.interface gtk-theme "macOS Sierra"
-gsettings set org.gnome.desktop.wm.preferences theme "macOS Sierra"
