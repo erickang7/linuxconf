eb73fb9f640dfe64db3743e0c8e7ad5c1f217a75
Eric Kang <erickang@bluewood>
2018-02-28 19:22:55 -0800

    .

--- suspend/tlp.sh
+++ suspend/tlp.sh
@@ -3,3 +3,10 @@ sudo apt-get install -y tlp tlp-rdw acpi-call
 sudo systemctl start tlp
 
 sudo cp sleep.conf /etc/systemd/system
+
+mkdir -p ~/tlpui
+cd ~/tlpui
+
+wget https://github.com/d4nj1/TLPUI/archive/master.zip
+unzip master.zip
+python3 tlpui.py
