0ff3c323e694fa4ec40a164c85616a4aea655a1f
erickang7 <erickang7@live.com>
2018-03-07 22:09:20 -0800

    .

--- archlinux/install/aur.sh
+++ archlinux/install/aur.sh
@@ -3,3 +3,7 @@ makepkg -si
 git clone https://aur.archlinux.org/packages/ttf-nanum/
 
 git clone https://aur.archlinux.org/ttf-unfonts-core-ibx.git 
+
+git clone https://aur.archlinux.org/ttf-google-fonts-git.git
+
+git clone https://aur.archlinux.org/ttf-monaco.git
