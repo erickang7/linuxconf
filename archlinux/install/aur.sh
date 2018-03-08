45e76d74f8b70622a5642dad000cfd6bc7ef94b8
erickang7 <erickang7@live.com>
2018-03-08 08:11:35 -0800

    .

--- archlinux/install/aur.sh
+++ archlinux/install/aur.sh
@@ -1,5 +1,6 @@
 makepkg -si
 
+#Fonts
 git clone https://aur.archlinux.org/packages/ttf-nanum/
 
 git clone https://aur.archlinux.org/ttf-unfonts-core-ibx.git 
@@ -7,3 +8,5 @@ git clone https://aur.archlinux.org/ttf-unfonts-core-ibx.git
 git clone https://aur.archlinux.org/ttf-google-fonts-git.git
 
 git clone https://aur.archlinux.org/ttf-monaco.git
+
+sudo pacman -S noto-fonts-emoji
