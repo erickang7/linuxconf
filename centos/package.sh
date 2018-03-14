56c869f0684627794a12b5ddd28e6cb834b84544
erickang7 <erickang7@live.com>
2018-03-13 20:33:59 -0700

    .'

---
+++ centos/package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum install -y vim wget curl java-1.8.0-openjdk-devel.x86_64 groupinstall development
+
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+
+sudo yum -y install python36u python36u-pip
--- createusb.sh
+++ createusb.sh
@@ -1,2 +1,3 @@
-dd bs=4M if=/path/to/archlinux.iso of=/dev/sdx status=progress oflag=sync
+sudo dd bs=4M if=/home/erickang/Downloads/CentOS-7-x86_64-Minimal-1708.iso \
+of=/dev/sda status=progress oflag=sync
 
