bb0ac5cb28f56c3b25a3ecfd4ddfb9cb4f489105
erickang7 <erickang7@live.com>
2018-03-13 20:59:46 -0700

    .

--- centos/package.sh
+++ centos/package.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-sudo yum install -y vim wget curl java-1.8.0-openjdk-devel.x86_64 groupinstall development
-
+sudo yum install -y vim wget curl java-1.8.0-openjdk-devel.x86_64 golang
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install python36u python36u-pip python36u-devel python36u-setuptools
+sudo pip3.6 install numpy pandas scipy scikit-learn cython matplotlib
 
-sudo yum -y install python36u python36u-pip
+
+#docker
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
