5a76f58de60d9e5ecb3fdda0b252dd907997bb41
erickang7 <erickang7@live.com>
2018-03-14 21:52:40 -0700

    .

--- archlinux/skype/aur.sh
+++ archlinux/skype/aur.sh
@@ -1,6 +1,8 @@
 git clone https://aur.archlinux.org/pidgin-hg.git
 git clone https://aur.archlinux.org/webkitgtk.git
 git clone https://aur.archlinux.org/gplugin.git
+git clone https://aur.archlinux.org/enchant1.6.git
+git clone https://aur.archlinux.org/meanwhile.git
 
 sudo pacman -S mercurial intltool meson gperf gobject-introspection cmake lua-lgi  
 
@@ -9,4 +11,6 @@ makepkg -si
 
 cd ../gplugin
 makepkg -si
+
+enchant1.6 meanwhile webkitgtk3
 
