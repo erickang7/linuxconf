5d5c639662461a58d940f9a61bf462359cf7b60c
erickang7 <erickang7@live.com>
2018-03-22 15:14:48 -0700

    .

--- archlinux/i3/install.sh
+++ archlinux/i3/install.sh
@@ -2,7 +2,5 @@ sudo pacman -S i3
 
 sudo pacman -S rxvt-unicode dmenu perl-json-xs perl-anyevent-i3 acpi playerctl
 alsa-utils sysstat feh conky terminator polkit
-#install xfce4-terminal
 
-#to enable volumne and backlight keys
 cp config ~/.config/i3/config
