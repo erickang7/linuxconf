6676c9c4d6d0999c885a8bb1d1c958da69f37514
erickang7 <erickang7@live.com>
2018-03-24 22:22:44 -0700

    .

--- archlinux/install/xserver.sh
+++ archlinux/install/xserver.sh
@@ -3,9 +3,9 @@
 pacman -S xorg-server xorg-apps xorg-xinit
 pacman -S xf86-video-intel xf86-video-vesa
 
-pacman -S gdm gnome
+#pacman -S gdm gnome
 #pacman -S gnome-extra
-
-systemctl enable gdm
+pacman -S lightdm lightdm-gtk-greeter 
+#systemctl enable gdm
 
 sudo reboot
