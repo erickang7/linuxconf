452727f81d850d005fd34feaa690a7dd057b6d35
erickang7 <erickang7@live.com>
2018-03-27 16:19:43 -0700

    .

--- archlinux/install/xserver.sh
+++ archlinux/install/xserver.sh
@@ -5,7 +5,10 @@ pacman -S xf86-video-intel xf86-video-vesa
 
 #pacman -S gdm gnome
 #pacman -S gnome-extra
-pacman -S lightdm lightdm-gtk-greeter 
+pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+git clone https://aur.archlinux.org/archlinux-artwork.git 
+
+
 #systemctl enable gdm
 
 sudo reboot
